2d5d991f22e30dea57c8b9218377b1cc4527c68c
marcCodez <marc_pulumbarit@hotmail.com>
2020-06-16 14:42:41 +1000

    Add item to ui

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const Item = function(id, name, calories){
 // All this data is hidden
 const data = {
     items: [
-        {id: 0, name: 'steak', calories: 1200},
-        {id: 1, name: 'egg', calories: 300},
-        {id: 2, name: 'rice', calories: 200}
+        // {id: 0, name: 'steak', calories: 1200},
+        // {id: 1, name: 'egg', calories: 300},
+        // {id: 2, name: 'rice', calories: 200}
     ],
     // currentItem = the item selcted to be updated
     currentItem: null,
@@ -29,6 +29,29 @@ return {
     getItems: function(){
         return data.items;
     },
+    addItem: function(name, calories){
+        let ID;
+       // Create ID to identify each item uniquely
+       if (data.items.length > 0) {
+           // make the index to be the length -1, e.g. if theres 4 items index will become 3
+           // then we want the ID of that particular item .id and add 1 to it, gives us a sense of auto-increment
+           // Basically getting the last item and adding 1 to the ID each time we add an item
+           ID = data.items[data.items.length - 1].id + 1;
+       } else {
+           ID = 0;
+       }
+
+       // Calories to number - parse it as a number becasue when we type it its a string
+       calories = parseInt(calories);
+
+       // Create new item
+       newItem = new Item(ID, name, calories);
+
+       // Add to items array - push the item to the Data structure above
+       data.items.push(newItem);
+       return newItem;
+    },
+    // Check our data structure on the console
   logData: function(){
       return data;
   }
@@ -44,7 +67,10 @@ const UIController = (function(){
     // class or ID can be changed at any time, and to save us from changing each individual instance of it
     // we can create the following object
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput:'#item-name',
+        itemCaloriesInput:'#item-calories'
     }
 
     // Public methods
@@ -57,7 +83,7 @@ const UIController = (function(){
                 html += `<li class="collection-item" id="item-${item.id}">
                 <strong>${item.name}: </strong> <em>${item.calories} Calories</em>  
                 <!-- secondary-content == a materialize class to add something to a collection like an icon or text -->
-                <!-- <a href="#" class="secondary-content">
+                 <a href="#" class="secondary-content">
                     <i class="edit-item fa fa-pencil"></i>
                 </a>
             </li>`
@@ -65,8 +91,44 @@ const UIController = (function(){
 
             // Insert list item
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function(){
+            return{
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        addListItem: function(item){
+            // Show the list
+            document.querySelector(UISelectors.itemList).style.display = 'block';
+            // Create li element
+            const li = document.createElement('li');
+            // Add class
+            li.className = 'collection-item';
+            // Add ID - its going to be dynamic, we can access it from the item object that we pass in that contains id, name and cals
+            li.id = `item-${item.id}`;
+            // Add HTML
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>  
+            <!-- secondary-content == a materialize class to add something to a collection like an icon or text --> 
+             <a href="#" class="secondary-content">
+                <i class="edit-item fa fa-pencil"></i>
+            </a>`;
+            // Insert item - adds it to the DOM
+            // insertAdjacentElment-inserts a given element node at a given position relative to the element it is invoked upon.
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+        },
+        clearInput: function(){
+            document.querySelector(UISelectors.itemNameInput).value = '';
+            document.querySelector(UISelectors.itemCaloriesInput).value = '';
+        },
+        hideList: function(){
+            // hide the list UI - get rid of the default grey line with no items
+            document.querySelector(UISelectors.itemList).style.display = 'none';
+        },
+        // Since UI selectors is not made public we need to return it here
+        getSelectors: function(){
+            return UISelectors;
         }
-
     }
 
 })();
@@ -77,6 +139,35 @@ const UIController = (function(){
 // App Controller
 //Insert the other controllers into the main controller
 const AppController = (function(ItemCtrl, UICtrl){
+    // Load Event listeners
+    const loadEventListeners = function(){
+        // Get UI Selectors
+        const UISelectors = UIController.getSelectors();
+
+        // Add Item Event
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
+    }
+
+    // Add Item submit
+    // need to return it with the init function to be called, since its private
+    const itemAddSubmit = function(e){
+        // Get form input from UI Controller
+        const input = UIController.getItemInput();
+        
+        // Check for name and calorie input
+        // .name and .calories from the UI controller
+        if (input.name !== '' && input.calories !== ''){
+            // Add item
+            const newItem = ItemController.addItem(input.name, input.calories);
+
+            // Add item to UI list
+            UIController.addListItem(newItem);
+
+            // Clear fields
+            UIController.clearInput();
+        }
+        e.preventDefault();
+    }
 
     // Public Methods
     // return initilizer function - anything we need to run when the application loads
@@ -87,8 +178,23 @@ const AppController = (function(ItemCtrl, UICtrl){
             // Fetch items from data structure
             const items = ItemCtrl.getItems();
 
+            // Check if any items
+            if(items.length === 0){
+                // need to unhide the list in the UI controller, since we never made it unhidden
+                UIController.hideList();
+            } else {
             // Populate list with items
-            UIController.populateItemList(items);
+            
+             UIController.populateItemList(items);
+
+            }
+
+            // copy this above
+            // // Populate list with items
+            // UIController.populateItemList(items);
+
+            // Load event listeners
+            loadEventListeners();
         }
     }
 
